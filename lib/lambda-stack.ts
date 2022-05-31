ad47f8437281ac727fd0d7438be16adadff594b6
Erik Dymbe <erik.dymbe@gmail.com>
2022-05-31 12:43:46 +0200

    Add userpool

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps, Duration, aws_lambda as lambda } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration, aws_lambda as lambda, aws_cognito as cognito } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
 
@@ -6,15 +6,32 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new PythonFunction(this, 'Lambda1', {
+    const lambda1 = new PythonFunction(this, 'Lambda1', {
       entry: './lambda-hooks/lambda1/src',
       runtime: lambda.Runtime.PYTHON_3_8,
       timeout: Duration.minutes(1)
     });
-    new PythonFunction(this, 'Lambda2', {
+    const lambda2 = new PythonFunction(this, 'Lambda2', {
       entry: './lambda-hooks/lambda2/src',
       runtime: lambda.Runtime.PYTHON_3_8,
       timeout: Duration.minutes(1)
+    });
+
+    const userpool = new cognito.UserPool(this, 'UserPool', {
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: false
+        }
+      },
+      signInAliases: {
+        email: true,
+        username: false
+      },
+      lambdaTriggers: {
+        postConfirmation: lambda1,
+        preTokenGeneration: lambda2
+      }
     });
   }
 }
