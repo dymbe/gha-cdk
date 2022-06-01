fe5eb10e63bb9fa9704d32832ed56c8ef5eb46f6
Erik Dymbe <erik.dymbe@gmail.com>
2022-06-01 12:13:44 +0200

    Add ecr-deploy

--- otherstack/other-stack.ts
+++ otherstack/other-stack.ts
@@ -3,8 +3,11 @@ import {
     StackProps,
     aws_servicediscovery as servicediscovery,
     aws_ec2 as ec2,
-    aws_ecs as ecs
+    aws_ecs as ecs,
+    aws_ecr as ecr,
+    aws_ecr_assets as ecrAssets
 } from 'aws-cdk-lib';
+import * as ecrDeploy from 'cdk-ecr-deployment';
 import { Construct } from 'constructs';
 
 export class OtherStack extends Stack {
@@ -12,11 +15,20 @@ export class OtherStack extends Stack {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'Vpc');
-    const cluster = new ecs.Cluster(this, "Cluster")
+    const cluster = new ecs.Cluster(this, 'Cluster')
 
     const namespace = new servicediscovery.PrivateDnsNamespace(this, 'Namespace', {
-      name: "namespaceName",
+      name: 'namespaceName',
       vpc: vpc
+    });
+
+    const userServiceRepo = new ecr.Repository(this, 'EcrRepo');
+
+    const dockerImageAsset = new ecrAssets.DockerImageAsset(this, 'ContainerImage', { directory: 'service' });
+
+    new ecrDeploy.ECRDeployment(this, 'DeployDockerImage', {
+      src: new ecrDeploy.DockerImageName(dockerImageAsset.imageUri),
+      dest: new ecrDeploy.DockerImageName(`${userServiceRepo.repositoryUri}:latest`)
     });
   }
 }
